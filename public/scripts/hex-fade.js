b7c3b2ee9479bcab14f1b53bf8a97af45810cdc8
Rebecca Weinberger <rew@mit.edu>
2017-07-13 00:59:44 +0200

    font and text adjustments

--- public/scripts/hex-fade.js
+++ public/scripts/hex-fade.js
@@ -3,12 +3,10 @@ $(document).ready(function(){
   $('.hexagon').hover(function() {
     id = this.id.substring(3);
     $('#hex'+id).animate({opacity:1}, fadeTime);
-    $('#img'+id).fadeOut();
     $('#text'+id).fadeIn()
   }, function() {
     id = this.id.substring(3);
     $('#hex'+id).animate({opacity:0.7}, fadeTime);
     $('#text'+id).fadeOut();
-    $('#img'+id).fadeIn()
   })
 });
