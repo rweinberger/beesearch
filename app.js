0cb97bd33663606d145e7084eee767065bac2a3d
Rebecca Weinberger <rew@mit.edu>
2017-08-01 14:48:01 +0200

    logging host

--- app.js
+++ app.js
@@ -87,7 +87,7 @@ app.get('/stream', function(req, res) {
 
 app.post('/test', function(req, res) {
   console.log('message from esp8266!');
-  console.log(req)
+  console.log('HOST: '+req.headers.host)
   // console.log(req)
 })
 
